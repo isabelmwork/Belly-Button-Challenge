27d787d8f8ff4dbd64afd0403ac536b8c3741a4d
isabelmwork <isabelmwork@gmail.com>
2023-09-21 21:32:27 -0700

    cleaned code' '

--- static/js/app2.js
+++ static/js/app2.js
@@ -12,92 +12,70 @@ dataSamples.then(function(data) {
     //samples contains sample_values, otu_ids
     let samples = data.samples;
 
-    let OTUs_vals = [];
-    let OTUs_ids = [];
-    let OTUs_labels = [];
-
+    //create dict that contains all the sample data
+    //all_dict[id] = []
+    let all_dict = {};
     for (row in samples) {
 
-        //store otu_ids
-        let row_otu_ids = samples[row].otu_ids;
-        OTUs_ids.push(row_otu_ids);
-
-        //store sample_values
-        let row_sample_values = samples[row].sample_values;
-        OTUs_vals.push(row_sample_values);
-
-        //store otu_labels
-        let row_labels= samples[row].otu_labels;
-        OTUs_labels.push(row_labels);
+        //grab row values, gets rid of keys
+        let row_values = Object.values(samples[row]);
+        //id
+        let row_id = row_values.slice(0,1);
+        //[otu_ids, sample_values, otu_labels]
+        let row_data = row_values.slice(1,7);
+        //all_dict[id] = [otu_ids, sample_values, otu_labels]
+        all_dict[row_id] = row_data;
     };
 
-    //sort by sample_values in desc order
+    let sorted_dict = {};
+    let top_10_dict = {};
+    //sort based on sample_values in desc order
     let samples_sorted = samples.sort((a,b) => b.sample_values - a.sample_values);
-    
-    //lists for storing data
-    let dropdown_ids = [];
-    let top_10_OTUs_ids = [];
-    let top_10_OTUs_vals = [];
-    let top_10_OTUs_labels = [];
-
-    //loop through the rows in samples
-    //populate lists
     for (row in samples_sorted) {
 
-        //store id
-        let id = samples_sorted[row].id;
-        dropdown_ids.push(id);
+        //grab row values, gets rid of keys
+        let row_values = Object.values(samples_sorted[row]);
+        //id
+        let row_id = row_values.slice(0,1);
+        //[otu_ids, sample_values, otu_labels]
+        let row_data = row_values.slice(1,7);
+        //all_dict[id] = [otu_ids, sample_values, otu_labels]
+        sorted_dict[row_id] = row_data;
 
-        //store top 10 otu_ids
-        let otu_ids_10 = samples_sorted[row].otu_ids.slice(0,10);
-        top_10_OTUs_ids.push(otu_ids_10);
+        let row_top_10 = [];
+        for (let i = 0; i < 3; i++) {
 
-        //store top 10 sample_values
-        let sample_values_10 = samples_sorted[row].sample_values.slice(0,10);
-        top_10_OTUs_vals.push(sample_values_10);
-
-        //store top 10 otu_lables
-        let otu_labels_10 = samples_sorted[row].otu_labels.slice(0,10);
-        top_10_OTUs_labels.push(otu_labels_10);
+            //grab top 10 for each (otu_ids, sample_values, otu_labels)
+            row_top_10.push(sorted_dict[row_id][i].slice(0,10));
+        };
+        //top_10_dict[id] = [otu_ids, sample_values, otu_labels]
+        top_10_dict[row_id] = row_top_10;
     };
 
-    //dict for storing lists
-    //used for easy look up when using dropdown menu
-    var all_dict = {};
-    var top_10_dict = {};
-    
-    //select id
-    //selector will store ids for dropdown menu
+    //grab all ids
+    let dropdown_ids = Object.keys(all_dict);
+    //grab selector
     let selector = d3.select("#selDataset");
-    //loop through ids
-    //populate dropdown menu and dict
+    //populate dropdown menu with ids
     for(let i =0; i < dropdown_ids.length; i++) {
         
-        //add id to dropdown menu
         selector.append("option").text(dropdown_ids[i]).property("value", dropdown_ids[i]);
         ////////////https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
-
-        //key = id
-        //value = [[otu_ids], [sample_values], [otu_labels]]
-        all_dict[dropdown_ids[i]] = [OTUs_ids[i], OTUs_vals[i], OTUs_labels[i]];
-        top_10_dict[dropdown_ids[i]] = [top_10_OTUs_ids[i], top_10_OTUs_vals[i], top_10_OTUs_labels[i]];
-    
     };
 
     //----------------------------------------------------------------------------
     //----- Bar Plot -----
     //----------------------------------------------------------------------------
 
-    //x = top_10_dict[id][sample_values]
-    //reverse so that graph has largest x on top
-    let bar_sample_values = top_10_dict[dropdown_ids[0]][1].reverse();
-    //y = top_10_dict[id][otu_ids]
-    //map so y = "OTU (otu_id)", for each otu_id
-    let bar_otu_ids = top_10_dict[dropdown_ids[0]][0].reverse().map(otu => `OTU ${otu}`);
+    //top_10_dict[id] = [otu_ids, sample_values, otu_labels]
+    //used id = 940 as the starter id for the website
+    //reverse each so that the largest value is on the top of the graph
 
-    let bar_otu_labels = top_10_dict[dropdown_ids[0]][2].reverse();
+    //make each otu_id display as "OTU otu_id"
+    let bar_otu_ids = top_10_dict[940][0].reverse().map(otu => `OTU ${otu}`);
+    let bar_sample_values = top_10_dict[940][1].reverse();
+    let bar_otu_labels = top_10_dict[940][2].reverse();
 
-    //populate website with bar plot of id = 940 (the first id in the dropdown menu)
     let bar_data = [{
     
         x: bar_sample_values,
@@ -112,9 +90,12 @@ dataSamples.then(function(data) {
     //----- Bubble Plot -----
     //----------------------------------------------------------------------------
     
-    let bubble_sample_values = all_dict[dropdown_ids[0]][1];
-    let bubble_otu_ids = all_dict[dropdown_ids[0]][0];
-    let bubble_otu_labels = all_dict[dropdown_ids[0]][2];
+    //want to display all the data (not just top 10) so used all_dict
+    //all_dict[id] = [otu_ids, sample_values, otu_labels]
+
+    let bubble_otu_ids = all_dict[940][0];
+    let bubble_sample_values = all_dict[940][1];
+    let bubble_otu_labels = all_dict[940][2];
     
     let bubble_data = [{
         
@@ -136,19 +117,28 @@ dataSamples.then(function(data) {
     //----- Sample Metadata -----
     //----------------------------------------------------------------------------
     
+    //grab metadata
     let metadata = data.metadata;
+    //grab first metadata (which has id = 940) to use as starter
     let sample_metadata = metadata[0];
 
     let metadata_keys = Object.keys(sample_metadata);
     let metadata_values = Object.values(sample_metadata);
 
     let metadata_text = "";
+    //want a string of the form:
+    //"key: value <br> key: value <br> ..."
     for (let i = 0; i < metadata_keys.length; i++) {
 
+        //create string of form:
+        //"key: value <br>"
         let key_value_text = `${metadata_keys[i]}: ${metadata_values[i]} <br>`;
+        //concat to add "key: value <br>"" to overall string
         metadata_text = metadata_text.concat(key_value_text);
     }
-    
+
+    //grab and populate sample_metadata
+    //convert string to html so that the <br>s work
     let select_sample_metadata =  d3.select("#sample-metadata");
     select_sample_metadata.html(metadata_text);
     //////////https://stackoverflow.com/questions/70926924/can-i-inject-br-tag-into-string-and-make-it-work-as-an-actual-html-tag
@@ -169,7 +159,7 @@ dataSamples.then(function(data) {
     //----- Update Plots -----
     //----------------------------------------------------------------------------
     
-    //update Plotly with x, y of new dropdown menu value
+    //update bar and bubble Plotlys with x, y, text of new dropdown menu value
     function updatePlotly() {
         
         //store dropdown_menu
@@ -178,16 +168,16 @@ dataSamples.then(function(data) {
         let selected_id = dropdown_menu.property("value");
 
         //same format as bar_sample_values, bar_otu_ids
-        let new_bar_sample_values = top_10_dict[selected_id][1].reverse();
         let new_bar_otu_ids = top_10_dict[selected_id][0].map(otu => `OTU ${otu}`).reverse();
+        let new_bar_sample_values = top_10_dict[selected_id][1].reverse();
         let new_bar_otu_labels = top_10_dict[selected_id][2].reverse();
-        //use restyle to update bar Plotly with new x,y values
+        //use restyle to update bar Plotly with new x,y values and text
         Plotly.restyle("bar", "x", [new_bar_sample_values]);
         Plotly.restyle("bar", "y", [new_bar_otu_ids]);
         Plotly.restyle("bar", "text", [new_bar_otu_labels]);
 
-        let new_bubble_sample_values = all_dict[selected_id][1];
         let new_bubble_otu_ids = all_dict[selected_id][0];
+        let new_bubble_sample_values = all_dict[selected_id][1];
         let new_bubble_otu_labels = all_dict[selected_id][2].reverse();
         Plotly.restyle("bubble", "x", [new_bubble_otu_ids]);
         Plotly.restyle("bubble", "y", [new_bubble_sample_values]);
@@ -208,22 +198,27 @@ dataSamples.then(function(data) {
     //----- Update Metadata -----
     //----------------------------------------------------------------------------
 
+    //create and populate dict with metadata info
+    //key = id
+    //values = ethnicity, gender, ...
     let metadata_dict = {};
     for (row in metadata) {
-
+        //id
         let meta_id =  Object.values(metadata[row]).slice(0,1)[0];
+        //other data (ethnicity, gender, ...)
         let meta_vals =  Object.values(metadata[row]).slice(1,7);
         metadata_dict[meta_id] = meta_vals;
     };
 
     function updateMetadata() {
-        
+        //grab new id
         let selected_id = d3.select("#selDataset").property("value");
+        //grab corresponding metadata
         let new_metadata = metadata_dict[selected_id];
+        //grab keys
         let key_names = Object.keys(metadata[0]);
 
-        //console.log(key_names);
-        //console.log(key_names.length);
+        //same concept as original metadata string
         let new_metadata_text = `${key_names[0]}: ${selected_id} <br>`;
         for (let i = 1; i < key_names.length; i++) {
 
@@ -232,17 +227,5 @@ dataSamples.then(function(data) {
         }
         
         select_sample_metadata.html(new_metadata_text);
-
-
     };
-
 });
-
-//FIX TEXT!!!
-
-
-//why doesnt this work?
-//console.log(samples.id);
-
-//variables only accessible inside function
-//other way?
